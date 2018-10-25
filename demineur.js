d637b9ed47ea3552a4708ab194810e31f0e4ed54
francisboudreau7 <francis.bdre@gmail.com>
2018-10-25 15:40:51 -0400

    Update demineur.js

--- demineur.js
+++ demineur.js
@@ -1,2 +1,9 @@
+
+
+
 load("images.js");
-afficherImage();
+var afficherImage = function(x,y, colormap, image);
+var attendreClic = function();
+var placerMines= function(largeur, hauteur, nbMines, x, y);
+var demineur= function(largeur, hauteur, nbMines);
+var testDemineur = function();
