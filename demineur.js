3c35455f947016ab30cfb3d8111d5f0c53dcf04e
francisboudreau7 <francis.bdre@gmail.com>
2018-10-26 16:20:06 -0400

    Update demineur.js

--- demineur.js
+++ demineur.js
@@ -3,14 +3,14 @@
 //2018-11-05
 load("untitled7.js");
 var afficherImage = function (x, y, colormap, image) {
-var hauteur=image.length;
-var largeur=image[1].length;
-for(var i=0;i<=hauteur-1;i++){
-    
-    for(var j=0;j<=largeur-1;j++){
-    setPixel(x+i,y+j,colormap[image[j][i]]);
+    var hauteur = image.length;
+    var largeur = image[1].length;
+    for (var i = 0; i <= hauteur - 1; i++) {
+
+        for (var j = 0; j <= largeur - 1; j++) {
+            setPixel(x + i, y + j, colormap[image[j][i]]);
+        }
     }
-}
 };
 var attendreClic = function () {
 
@@ -21,19 +21,32 @@ var attendreClic = function () {
     return (souris.x, souris.y);
 
 };
-var placerMines = function (Math.floor(largeur/image.length),
- Math.floor(hauteur/image[1].length), nbMines, x, y) {
-var mines= Array(largeur,hauteur);
-while(nbMine>0){
-var case = {x:Math.floor(Math.random()*largeur),y:Math.floor(Math.random()*hauteur)}
-if(mines[case.x][case.y]==false)
-&& Math.floor(x/largeur)!=case.x 
-&&Math.floor(y/hauteur)!=case.y{
-    mines[case.x][case.y]=true;
-    nbMines-=1;
-}
-}
-    
+var placerMines = function (Math.floor(largeur / image.length),
+    Math.floor(hauteur / image[1].length), nbMines, x, y) {
+    var mines = Array(largeur, hauteur);
+    while (nbMine > 0) {
+        var
+        case = {
+            x: Math.floor(Math.random() * largeur),
+            y: Math.floor(Math.random() * hauteur)
+        }
+        if (mines[
+                case.x][
+                case.y
+            ] == false)
+            &&
+            Math.floor(x / largeur) !=
+            case.x &&
+            Math.floor(y / hauteur) !=
+            case.y {
+                mines[
+                    case.x][
+                    case.y
+                ] = true;
+                nbMines -= 1;
+            }
+    }
+
 };
 
 var demineur = function (largeur, hauteur, nbMines) {};
